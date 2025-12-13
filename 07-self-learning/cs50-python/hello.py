bcb18fda10dc0220420b131736e2bf0668bb341e
Yunqi Ji <yunqiji.dev@gmail.com>
2025-12-13 22:14:18 +0800

    feat(self-learning): refine hello.py and add new exercises for CS50P

--- 07-self-learning/cs50-python/hello.py
+++ 07-self-learning/cs50-python/hello.py
@@ -10,41 +10,39 @@ print(name)
 
 
 # Exercise 3 (commenting code)
-#Ask for user's name 
+# Ask for user's name
 
 
 # Say hello to user
-
 
 
 # Exercise 4 (string concatenation)
-# Ask for user's name 
+# Ask for user's name
 name = input("What's your name? ")
 
 # Say hello to user
-print("hello, "+ name)
+print("hello, " + name)
 
 
-#Exercise 5 (argument separation)
-# Ask for user's name   
+# Exercise 5 (argument separation)
+# Ask for user's name
 name = input("What's your name? ")
 
 # Say hello to user
 print("hello,", name)
 
 
-
 # Exercise 6 (end parameter)
-#Ask for user's name 
+# Ask for user's name
 name = input("What's your name? ")
 
 # Say hello to user
-print("hello, ", end= "")
+print("hello, ", end="")
 print(name)
 
 
 # Exercise 7 (sep parameter)
-#Ask for user's name
+# Ask for user's name
 name = input("What's your name? ")
 
 # Say hello to user
@@ -52,15 +50,15 @@ print("hello", name, sep=", ")
 
 
 # Exercise 8 (f-strings)
-#Ask for user's name
+# Ask for user's name
 name = input("What's your name? ")
 
 # Say hello to user
-print(f"hello, {name}") 
+print(f"hello, {name}")
 
 
 # Exercise 9 (removing whitespace)
-#Ask for user's name
+# Ask for user's name
 name = input("What's your name? ")
 
 # Remove whitespace from string
@@ -71,7 +69,7 @@ print(f"hello, {name}")
 
 
 # Exercise 10 (capitalizing names)
-#Ask for user's name
+# Ask for user's name
 name = input("What's your name? ")
 
 # Remove whitespace from string
@@ -85,10 +83,10 @@ print(f"hello, {name}")
 
 
 # Exercise 11 (title casing names)
-#Ask for user's name
+# Ask for user's name
 name = input("What's your name? ")
 
-# Remove whitespace from string and 
+# Remove whitespace from string and capitalize user's name
 name = name.strip().title
 
 # Say hello to user
@@ -96,13 +94,43 @@ print(f"hello, {name}")
 
 
 # Exercise 12 (splitting names)
-#Ask for user's name
+# Ask for user's name
 name = input("What's your name? ").strip().title()
 
 # Split the name into first and last names
-first,last = name.split(" ")
+first, last = name.split(" ")
 
 # Say hello to user
 print(f"hello, {first}")
 
 
+# Exercise 13 (defining functions)
+def hello(to):
+    print("hello,", to)
+
+
+name = input("What's your name? ")
+hello(name)
+
+
+# Exercise 14 (default parameters)
+def hello(to="world"):
+    print(f"hello, {to}")
+
+
+hello()
+name = input("What's your name? ")
+hello(name)
+
+
+# Exercise 15 (main function)
+def main():
+    name = input("What's your name? ")
+    hello(name)
+
+
+def hello(to="world"):
+    print(f"hello, {to}")
+
+
+main()
